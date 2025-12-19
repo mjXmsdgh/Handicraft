f5b19dc7dcc6db585e0dd447ff33c1325e5862a5
mjXmsdgh <masudayouichi41@gmail.com>
2025-12-19 20:15:28 +0900

    add robots.txt

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,3 +32,28 @@ DEFAULT_PAGINATION = 10
 
 # テーマを設定
 THEME = 'theme/elegant'
+
+
+# プラグイン
+PLUGINS = ['pelican.plugins.sitemap']
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities':{
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+        },
+    'changefreqs':{
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+        }
+    
+    }
+
+# robot.txt
+STATIC_PATH = ['images','robots.txt']
+EXTRA_PATH_METADATA = {
+    'robots.txt':{'path':'robots.txt'},
+    }
